d07a300117f6fdd0446fbdcbd4481a0c417fd3b6
balakrishdq <dqsoft0601@gmail.com>
2023-01-10 21:28:30 +0530

    firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/firebase_options.dart';
 import 'package:quiz_app/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
